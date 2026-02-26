7cbcfd481eda3cf99608d993ad79e1e0fcc75e8a
chenyomi <408550179@qq.com>
2026-02-26 15:35:38 +0800

    fix: 更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  base: 'leafer-htmltext-edit-view',
-  plugins: [vue()],
-  resolve: {
-    dedupe: [
-      '@leafer-ui/core',
-      '@leafer-in/editor', 
-      '@leafer-in/html',
-      'leafer-ui'
-    ]
-  },
-  server: {
-    port: 80,
-    host: '0.0.0.0',
-    strictPort: true,
-    allowedHosts: true
-  },
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve'
+
+  return {
+    base: 'leafer-htmltext-edit-view',
+    plugins: [vue()],
+    resolve: {
+      dedupe: isDev
+        ? ['@leafer-ui/core', '@leafer-in/editor', '@leafer-in/html', 'leafer-ui']
+        : []
+    },
+    server: {
+      port: 80,
+      host: '0.0.0.0',
+      strictPort: true,
+      allowedHosts: true
+    }
+  }
 })
